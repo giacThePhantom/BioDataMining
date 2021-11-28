fc62fbb192a2bf8ac9837f216d38ae79163207db
GaiaFaggin <62758569+GaiaFaggin@users.noreply.github.com>
2021-11-28 16:51:05 +0100

    Create rma.r

---
+++ src/normalization/rma.r
@@ -0,0 +1,19 @@
+library(GEOquery)
+library(affy)
+library(hgu133a.db)
+library(hgu133acdf)
+
+setwd("//percorso//file//CEL")
+
+f <- list.files(pattern = ".CEL")
+
+i = 0
+j = 0
+
+for(i = 1, i <= j, j++ in 1:number of file){ // set number of file in the folder
+  raw.data <- ReadAffy(verbose = FALSE, filenames = f[i])
+  data.rma.norm = rma(raw.data)
+}
+
+rma = exprs(data.rma.norm)
+write.table(rma, file="rma.txt", quote=FALSE, sep="\t")
